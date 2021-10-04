60e0cc9c3ff45475cec758f3487634517e0e848b
anthonynelson2021 <Si4m3seC4t>
2021-10-04 18:28:47 -0400

    Added Help Screen

---
+++ app/src/main/java/com/example/seniorshelpingseniors/HelpScreen.java
@@ -0,0 +1,14 @@
+package com.example.seniorshelpingseniors;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_screen);
+    }
+}
